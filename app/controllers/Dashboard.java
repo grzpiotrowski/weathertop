024d4f9623003552f3a828da1adf7b3aa1a24828
grzpiotrowski <geopiotrowski@gmail.com>
2022-05-14 00:42:24 +0100

    Added user accounts functionality, signup, login

--- app/controllers/Dashboard.java
+++ app/controllers/Dashboard.java
@@ -1,5 +1,6 @@
 package controllers;
 
+import models.Member;
 import models.Station;
 import play.Logger;
 import play.mvc.Controller;
@@ -10,7 +11,8 @@ public class Dashboard extends Controller
 {
   public static void index() {
     Logger.info("Rendering Dashboard");
-    List<Station> stations = Station.findAll();
+    Member member = Accounts.getLoggedInMember();
+    List<Station> stations = member.stations;
 
     render ("dashboard.html", stations);
   }
@@ -21,8 +23,10 @@ public class Dashboard extends Controller
    */
   public static void addStation(String name)
   {
-    Station station = new Station(name);
     Logger.info("Adding a new station: " + name);
+    Member member = Accounts.getLoggedInMember();
+    Station station = new Station(name);
+    member.stations.add(station);
     station.save();
     redirect("/dashboard");
   }
---
+++ app/models/Member.java
@@ -0,0 +1,41 @@
+package models;
+
+import play.db.jpa.Model;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import java.util.List;
+import java.util.ArrayList;
+
+
+@Entity
+public class Member extends Model
+{
+  public String firstName;
+  public String lastName;
+  public String email;
+  public String password;
+
+  @OneToMany(cascade = CascadeType.ALL)
+  public List<Station> stations = new ArrayList<Station>();
+
+  public Member(String firstName, String lastName, String email, String password)
+  {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.email = email;
+    this.password = password;
+  }
+
+  public static Member findByEmail(String email)
+  {
+    return find("email", email).first();
+  }
+
+  public boolean checkPassword(String password)
+  {
+    return this.password.equals(password);
+  }
+
+}
